01177aaaed344fc16fe19baab5f3ebf56ca14460
LaoQu <1157821899@qq.com>
2020-07-24 15:34:28 +0800

    首页模块-头部内容布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,27 @@
 <template>
-    <el-container class="container">
-      <el-aside width="200px">Aside</el-aside>
-      <el-container>
-        <el-header>Header</el-header>
-        <el-main>Main</el-main>
-      </el-container>
+  <el-container class="container">
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>
+        <!-- 左侧 字体图标和文本说明-->
+        <span class="el-icon-s-unfold icon"></span>
+        <span class="text">后台管理系统</span>
+        <!-- 右侧 下拉菜单-->
+        <el-dropdown class="dropdown">
+          <span class="el-dropdown-link">
+            <img src="../../assets/aixinxin.jpg" class="img" alt />
+            <span class="wenben">用户名称</span>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-unlock">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+      <el-main>Main</el-main>
     </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -13,17 +29,42 @@ export default {}
 </script>
 
 <style lang="less" scoped>
-.container{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    .el-aside{
-        background: aquamarine;
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  .el-aside {
+    background: #424242;
+  }
+  .el-header {
+    border: 1px solid #ddd;
+    line-height: 60px;
+    .icon {
+      font-size: 24px;
+      vertical-align: middle;
+      //   color: palevioletred;
     }
-    .el-header{
-        border: 1px solid #ddd;
+    .text {
+      //   color: palevioletred;
+      font-size: 16px;
+      vertical-align: middle;
+      padding-left: 10px;
     }
+    .dropdown {
+      float: right;
+      .img {
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .wenben {
+        font-weight: bolder;
+        vertical-align: middle;
+        padding-left: 10px;
+      }
+    }
+  }
 }
 </style>

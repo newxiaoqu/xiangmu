6d5fddafd65d7767e2b99dd4a07f49fd823fdb44
LaoQu <1157821899@qq.com>
2020-07-24 21:05:33 +0800

    首页模块-导航菜单路由功能开启

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -26,34 +26,35 @@
         active-text-color="#ffd04b"
         :collapse="!isOpen"
         :collapse-transition = 'false'
+        router
       >
         <!-- el-submenu有子菜单的导航组件 -->
         <!-- el-menu-item没有子菜单的一级导航组件 -->
-        <el-menu-item index="1">
+        <el-menu-item index="/">
           <i class="el-icon-s-home"></i>
           <span slot="title">首页</span>
         </el-menu-item>
-        <el-menu-item index="2">
+        <el-menu-item index="/article">
           <i class="el-icon-document"></i>
           <span slot="title">内容管理</span>
         </el-menu-item>
-        <el-menu-item index="3">
+        <el-menu-item index="/image">
           <i class="el-icon-picture"></i>
           <span slot="title">素材管理</span>
         </el-menu-item>
-        <el-menu-item index="4">
+        <el-menu-item index="/publish">
           <i class="el-icon-s-promotion"></i>
           <span slot="title">发布文章</span>
         </el-menu-item>
-        <el-menu-item index="5">
+        <el-menu-item index="/comment">
           <i class="el-icon-chat-dot-round"></i>
           <span slot="title">评论管理</span>
         </el-menu-item>
-        <el-menu-item index="6">
+        <el-menu-item index="/fans">
           <i class="el-icon-present"></i>
           <span slot="title">粉丝管理</span>
         </el-menu-item>
-        <el-menu-item index="7">
+        <el-menu-item index="/setting">
           <i class="el-icon-setting"></i>
           <span slot="title">个人设置</span>
         </el-menu-item>
@@ -62,7 +63,7 @@
     <el-container>
       <el-header>
         <!-- 左侧 字体图标和文本说明-->
-        <span @click="toggerAside" class="el-icon-s-unfold icon"></span>
+        <span @click="toggerAside" class="el-icon-s-fold icon"></span>
         <span class="text">后台管理系统</span>
         <!-- 右侧 下拉菜单-->
         <el-dropdown class="dropdown">

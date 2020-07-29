4576d0f9755bc66829ab7a1fff901deb46b9292c
LaoQu <1157821899@qq.com>
2020-07-29 19:04:56 +0800

    配置了axios-请求拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,12 @@ import store from '@/store'
 // 2.默认配置
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 
-// 3.默认配置
-axios.defaults.headers.Authorization = `Bearer ${store.getUser().token}`
+// axios.defaults.headers.Authorization = `Bearer ${store.getUser().token}`
+
+// 3.请求拦截器
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${store.getUser().token}`
+  return config
+}, err => Promise.reject(err))
 
 export default axios
